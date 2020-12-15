4b13b24ea9fb7ebf2bc3c2869357aa6fc9e000b8
Christywu16 <wumengqi@umich.edu>
2020-12-15 10:21:20 +0800

    modified docu table4

--- Start.py
+++ Start.py
@@ -23,54 +23,54 @@ if __name__ == "__main__":
     tb_Covid1 = "CovidData1"
     # Get Stock Data
     Stock = GetStockData(cache_stock)
-    # Stock.get_data_with_caching()
+    Stock.get_data_with_caching()
     # Get Weather Data
     Weather = GetWeatherData(cache_weather)
-    # Weather.get_data_with_caching()
+    Weather.get_data_with_caching()
     # Get Covid Data
     Covid = GetCovidData(cache_Covid)
-    # Covid.get_data_with_caching()
+    Covid.get_data_with_caching()
     # Write data into database
     writer = WriteData(db_name, tb_stock, tb_weather, tb_Covid, tb_Covid1)
     cur, conn = writer.SetUpDatabase()
     writer.SetUpTable(cache_stock, cache_weather, cache_Covid, cur, conn)
     # Get the correlation coefficients
     stock_dic = Cache.read_cache(cache_stock)
-    # if stock_dic.get('2020-09-25', 0) != 0:
+    if stock_dic.get('2020-09-25', 0) != 0:
         # Correlation Coefficient 0
-        # correl0 = writer.correl('temp', 'WeatherData',
-        #                         'closing_price', 'StockData', cur, conn)
-        # str0 = "Correlation coefficient between temperature and Apple's stock price is " + \
-        #     str(correl0)
-        # writer.write_correl('output.txt', str0)
-        # # Correlation Coefficient 1
-        # correl1 = writer.correl(
-        #     'positiveIncrease', 'CovidData', 'closing_price', 'StockData', cur, conn)
-        # str1 = "Correlation coefficient between daily positive cases and Apple's stock price is " + \
-        #     str(correl1)
-        # writer.write_correl('output.txt', str1)
-        # # Correlation Coefficient 2
-        # correl2 = writer.correl(
-        #     'deathIncrease', 'CovidData', 'closing_price', 'StockData', cur, conn)
-        # str2 = "Correlation coefficient between daily death cases and Apple's stock price is " + \
-        #     str(correl2)
-        # writer.write_correl('output.txt', str2)
-        # # Correlation Coefficient 3
-        # correl3 = writer.correl(
-        #     'hospitalizedIncrease', 'CovidData', 'closing_price', 'StockData', cur, conn)
-        # str3 = "Correlation coefficient between daily hospitalized cases and Apple's stock price is " + \
-        #     str(correl3)
-        # writer.write_correl('output.txt', str3)
+        correl0 = writer.correl('temp', 'WeatherData',
+                                'closing_price', 'StockData', cur, conn)
+        str0 = "Correlation coefficient between temperature and Apple's stock price is " + \
+            str(correl0)
+        writer.write_correl('output.txt', str0)
+        # Correlation Coefficient 1
+        correl1 = writer.correl(
+            'positiveIncrease', 'CovidData', 'closing_price', 'StockData', cur, conn)
+        str1 = "Correlation coefficient between daily positive cases and Apple's stock price is " + \
+            str(correl1)
+        writer.write_correl('output.txt', str1)
+        # Correlation Coefficient 2
+        correl2 = writer.correl(
+            'deathIncrease', 'CovidData', 'closing_price', 'StockData', cur, conn)
+        str2 = "Correlation coefficient between daily death cases and Apple's stock price is " + \
+            str(correl2)
+        writer.write_correl('output.txt', str2)
+        # Correlation Coefficient 3
+        correl3 = writer.correl(
+            'hospitalizedIncrease', 'CovidData', 'closing_price', 'StockData', cur, conn)
+        str3 = "Correlation coefficient between daily hospitalized cases and Apple's stock price is " + \
+            str(correl3)
+        writer.write_correl('output.txt', str3)
         # Visualization
-        # writer.viz('temp', 'weatherData',
-        #             'closing_price', 'StockData', cur, conn, 'Daily Temperature',
-        #             "Apple stock's closing price", '''Daily Temperature vs. Apple Stock's Closing Price''')
-        # writer.viz('positiveIncrease', 'CovidData',
-        #            'closing_price', 'StockData', cur, conn, 'Daily Covid Positive Increase',
-        #            "Apple stock's closing price", '''Daily Covid Positive Increase vs. Apple Stock's Closing Price''')
-        # writer.viz('deathIncrease', 'CovidData',
-        #            'closing_price', 'StockData', cur, conn, 'Daily Covid Death Increase',
-        #            "Apple stock's closing price", '''Daily Covid Death Increase vs. Apple Stock's Closing Price''')
-        # writer.viz('hospitalizedIncrease', 'CovidData',
-        #            'closing_price', 'StockData', cur, conn, 'Daily Covid Hospitalized Increase',
-        #            "Apple stock's closing price", '''Daily Covid Hospitalized Increase vs. Apple Stock's Closing Price''')
+        writer.viz('temp', 'weatherData',
+                    'closing_price', 'StockData', cur, conn, 'Daily Temperature',
+                    "Apple stock's closing price", '''Daily Temperature vs. Apple Stock's Closing Price''')
+        writer.viz('positiveIncrease', 'CovidData',
+                   'closing_price', 'StockData', cur, conn, 'Daily Covid Positive Increase',
+                   "Apple stock's closing price", '''Daily Covid Positive Increase vs. Apple Stock's Closing Price''')
+        writer.viz('deathIncrease', 'CovidData',
+                   'closing_price', 'StockData', cur, conn, 'Daily Covid Death Increase',
+                   "Apple stock's closing price", '''Daily Covid Death Increase vs. Apple Stock's Closing Price''')
+        writer.viz('hospitalizedIncrease', 'CovidData',
+                   'closing_price', 'StockData', cur, conn, 'Daily Covid Hospitalized Increase',
+                   "Apple stock's closing price", '''Daily Covid Hospitalized Increase vs. Apple Stock's Closing Price''')
